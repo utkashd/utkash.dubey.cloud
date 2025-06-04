5ba89209749aca16fd305677bfa1d713fedd6e29
Utkash Dubey <utkashd@gmail.com>
2025-06-04 02:33:59 -0700

    debug?

--- utkash-dubey-cloud/functions/media/[[all]].js
+++ utkash-dubey-cloud/functions/media/[[all]].js
@@ -1,4 +1,7 @@
 export async function onRequestGet(ctx) {
+    // This was created based on instructions for serving R2 assets:
+    // https://developers.cloudflare.com/pages/tutorials/use-r2-as-static-asset-storage-for-pages/#serve-r2-assets-from-pages
+    // The actual media live in an R2 Bucket in my Cloudflare account
     console.log("hi!");
     const path = new URL(ctx.request.url).pathname.replace("/media/", "");
     const file = await ctx.env.MEDIA.get(path);
